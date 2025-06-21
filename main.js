1058ffbe262e160a3c1b8b9025bb8059a1fb50af
hanyangzhoupiano <130773491+hanyangzhoupiano@users.noreply.github.com>
2025-06-20 22:36:38 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,71 @@
+class $Wrapper {
+    constructor(input) {
+        if (typeof input === "string") {
+            this.elements = Array.from(document.querySelectorAll(input));
+        } else if (input instanceof Element) {
+            this.elements = [input];
+        } else if (input instanceof NodeList || Array.isArray(input)) {
+            this.elements = Array.from(input);
+        } else {
+            this.elements = [];
+        }
+    }
+
+    on(events, callback) {
+        this.elements.forEach(el =>
+            events.split(" ").forEach(evt => el.addEventListener(evt, callback))
+        );
+        return this;
+    }
+
+    each(callback) {
+        this.elements.forEach((el, i) => callback.call(el, i, el));
+        return this;
+    }
+
+    css(attr, value) {
+        this.elements.forEach(el => el.style[attr] = value);
+        return this;
+    }
+
+    html(content) {
+        this.elements.forEach(el => el.innerHTML = content);
+        return this;
+    }
+
+    text(content) {
+        this.elements.forEach(el => el.textContent = content);
+        return this;
+    }
+
+    attr(name, value) {
+        if (value !== undefined) {
+            this.elements.forEach(el => el.setAttribute(name, value));
+            return this;
+        } else {
+            return this.elements.map(el => el.getAttribute(name));
+        }
+    }
+
+    addClass(name) {
+        this.elements.forEach(el => el.classList.add(name));
+        return this;
+    }
+
+    removeClass(name) {
+        this.elements.forEach(el => el.classList.remove(name));
+        return this;
+    }
+
+    toggleClass(name) {
+        this.elements.forEach(el => el.classList.toggle(name));
+        return this;
+    }
+
+    $(selector) {
+        let descendants = this.elements.flatMap(el => Array.from(el.querySelectorAll(selector)));
+        return new $Wrapper(descendants);
+    }
+}
+
+const $ = (selector) => new $Wrapper(selector);
